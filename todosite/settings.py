2395456f2db26dfea57360e5a4a87de9ee6c3c93
emanuel <ecastillo76@misena.edu.co>
2022-06-05 12:36:31 -0500

    Update workflow file

--- todosite/settings.py
+++ todosite/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = True
 
 TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
 
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'todosite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': config('DB_ENGINE'),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        "PASSWORD": os.environ.get('DB_PASSWORD'),
+        "HOST": config('DB_HOST'),
+        "PORT": config('DB_PORT')
     }
 }
 
@@ -115,12 +115,12 @@ USE_I18N = True
 USE_TZ = True
 
 # EMAIL CONFIG
-EMAIL_FROM_USER         = config('EMAIL_FROM_USER')
-EMAIL_HOST              = config('EMAIL_HOST')
-EMAIL_HOST_USER         = config('EMAIL_FROM_USER')
-EMAIL_HOST_PASSWORD     = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS           = config('EMAIL_USE_TLS')
-EMAIL_PORT              = config('EMAIL_PORT')
+EMAIL_FROM_USER = config('EMAIL_FROM_USER')
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = config('EMAIL_FROM_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
 
 
 
